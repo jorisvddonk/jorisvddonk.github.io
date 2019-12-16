10d9e4072f0dda56346dbf948d382e43c3ef92fe
jorisvddonk <jorisvddonk@users.noreply.github.com>
2019-12-16 19:34:09 +0000

    deployed via Gatsby Publish Action 🎩 for 1f1b825ae8427a95d738c56e1e16a80695d0d6b0

---
+++ component---src-pages-404-tsx-bad5abf7b950b35aee81.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"9Dj+":function(e,n,t){"use strict";var a=t("q1tI"),l=t.n(a);t("8ypT");n.a=function(e){var n=e.children;return l.a.createElement(l.a.Fragment,null,l.a.createElement("header",null),l.a.createElement("main",null,n),l.a.createElement("footer",null))}},"i6+/":function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),r=t("9Dj+");n.default=function(){return l.a.createElement(r.a,null,l.a.createElement("h1",null,"NOT FOUND"))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-bad5abf7b950b35aee81.js.map
